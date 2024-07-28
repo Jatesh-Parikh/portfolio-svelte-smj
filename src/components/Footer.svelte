1b6b46301824caba73a9243564f1088d7c7254df
Jatesh Parikh <jateshparikh97@gmail.com>
2024-07-28 07:35:57 +0530

    footer done

---
+++ src/components/Footer.svelte
@@ -0,0 +1,22 @@
+<footer class="py-20 sm:py-32 border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 items-center justify-center">
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium">Connect with me &darr;</p>
+    <div class="flex flex-col gap-4 items-center justify-center">
+        <p><b class="pr-2">Email</b>
+            <a href="mailto:jateshparikh97@gmail.com" target="_blank" class="text-violet-400 duration-200 hover:opacity-80">
+                 jateshparikh97@gmail.com
+            </a>
+        </p>
+        <p>
+            <b class="pr-2">Github</b>
+            <a href="https://github.com/Jatesh-Parikh" target="_blank" class="text-violet-400 duration-200 hover:opacity-80">
+                Jatesh Parikh <sup><span><i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i></span></sup>
+            </a>
+        </p>
+        <p>
+            <b class="pr-2">Linkedin</b>
+             <a href="https://www.linkedin.com/in/jatesh-parikh/" target="_blank" class="text-violet-400 duration-200 hover:opacity-80">
+                Jatesh Parikh <sup><span><i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i></span></sup>
+            </a>
+        </p>
+    </div>
+</footer>
